b2acf59edc3af275f583d302651e9e4e07dbae14
Max Fichtelmann <max.fichtelmann@gmail.com>
2014-10-24 18:29:34 +0200

    add possibility to attach ECP functionality to existing HttpClientBuilder configurations

--- src/main/java/de/tudarmstadt/ukp/shibhttpclient/ShibHttpClientFactory.java
+++ src/main/java/de/tudarmstadt/ukp/shibhttpclient/ShibHttpClientFactory.java
@@ -1,7 +1,14 @@
 package de.tudarmstadt.ukp.shibhttpclient;
 
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
 import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.opensaml.xml.parse.BasicParserPool;
+
+import de.tudarmstadt.ukp.shibhttpclient.authentication.Authenticator;
+import de.tudarmstadt.ukp.shibhttpclient.processor.EcpRequestPreProcessor;
+import de.tudarmstadt.ukp.shibhttpclient.processor.EcpResponsePostProcessor;
 
 public class ShibHttpClientFactory
 {
@@ -100,5 +107,47 @@ public class ShibHttpClientFactory
     public HttpClient create( String idpUrl, String username, String password, HttpHost proxy, boolean anyCert, boolean transparentAuth )
     {
         return new ShibHttpClient( idpUrl, username, password, proxy, anyCert, transparentAuth );
+    }
+    
+    /**
+     * Attach ECP interceptors to the given {@link HttpClientBuilder} using the specified idpUrl and {@link Authenticator}. A
+     * {@link HttpClient} based on the current {@link HttpClientBuilder} configuration is used to send requests to IdP/SP.
+     * 
+     * @param clientBuilder
+     *            the build for the ecp-intercepted {@link HttpClient}
+     * @param idpUrl
+     *            the URL of the IdP. Should probably be something ending in "/SAML2/SOAP/ECP"
+     * @param authenticator
+     *            the {@link Authenticator} to provide credentials to a {@link HttpRequest} (while logging in at the IdP.
+     * @return a {@link HttpClientBuilder} based on the given configuration + ecp interceptors for transparent authentication.
+     */
+    public HttpClientBuilder attachEcpInterceptors( HttpClientBuilder clientBuilder, String idpUrl, Authenticator authenticator )
+    {
+        return attachEcpInterceptors( clientBuilder, clientBuilder.build(), idpUrl, authenticator );
+    }
+    
+    /**
+     * Attach ECP interceptors to the given {@link HttpClientBuilder} using the specified idpUrl and {@link Authenticator}.
+     * 
+     * @param clientBuilder
+     *            the build for the ecp-intercepted {@link HttpClient}
+     * @param ecpClient
+     *            the {@link HttpClient} that is used to execute the transparent requests.
+     * @param idpUrl
+     *            the URL of the IdP. Should probably be something ending in "/SAML2/SOAP/ECP"
+     * @param authenticator
+     *            the {@link Authenticator} to provide credentials to a {@link HttpRequest} (while logging in at the IdP.
+     * @return a {@link HttpClientBuilder} based on the given configuration + ecp interceptors for transparent authentication.
+     */
+    public HttpClientBuilder attachEcpInterceptors( HttpClientBuilder clientBuilder, HttpClient ecpClient, String idpUrl,
+            Authenticator authenticator )
+    {
+        BasicParserPool parserPool = new BasicParserPool();
+        parserPool.setNamespaceAware( true );
+        
+        EcpRequestPreProcessor preProcessor = new EcpRequestPreProcessor( ecpClient );
+        EcpResponsePostProcessor postProcessor = new EcpResponsePostProcessor( ecpClient, parserPool, idpUrl, authenticator );
+        
+        return clientBuilder.addInterceptorFirst( preProcessor ).addInterceptorLast( postProcessor );
     }
 }
