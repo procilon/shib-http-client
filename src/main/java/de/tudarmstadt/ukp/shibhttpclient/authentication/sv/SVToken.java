81f5a19ebd3c39f00a97ebfa77bcd7f25b3efbc1
Max Fichtelmann <max.fichtelmann@gmail.com>
2014-11-04 09:08:40 +0100

    add unmarshalling methods to ASN1 structures

--- src/main/java/de/tudarmstadt/ukp/shibhttpclient/authentication/sv/SVToken.java
+++ src/main/java/de/tudarmstadt/ukp/shibhttpclient/authentication/sv/SVToken.java
@@ -1,6 +1,11 @@
 package de.tudarmstadt.ukp.shibhttpclient.authentication.sv;
 
+import java.text.ParseException;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.DEREncodable;
 import org.bouncycastle.asn1.DERObject;
 import org.bouncycastle.asn1.DEROctetString;
@@ -40,5 +45,71 @@ public class SVToken implements DEREncodable
         vector.add( new DEROctetString( signature ) );
         
         return new DERSequence( vector );
+    }
+    
+    public static SVToken parse( ASN1Sequence sequence ) throws ParseException
+    {
+        if ( sequence.size() != 4 )
+        {
+            throw new IllegalArgumentException( "expected sequence of size 4" );
+        }
+        
+        DEREncodable encodedData = sequence.getObjectAt( 0 );
+        DEREncodable encodedSigner = sequence.getObjectAt( 1 );
+        DEREncodable encodedAlgorithm = sequence.getObjectAt( 2 );
+        DEREncodable encodedSignature = sequence.getObjectAt( 3 );
+        
+        TokenData data = TokenData.parse( DERSequence.getInstance( encodedData ) );
+        IssuerSerial signer = IssuerSerial.getInstance( encodedSigner );
+        AlgorithmIdentifier algorithm = AlgorithmIdentifier.getInstance( encodedAlgorithm );
+        byte[] signature = DEROctetString.getInstance( encodedSignature ).getOctets();
+        
+        return new SVToken( data, signer, algorithm, signature );
+    }
+    
+    /**
+     * Get the data.
+     * 
+     * @return the data
+     */
+    public TokenData getData()
+    {
+        return data;
+    }
+    
+    /**
+     * Get the signer.
+     * 
+     * @return the signer
+     */
+    public IssuerSerial getSigner()
+    {
+        return signer;
+    }
+    
+    /**
+     * Get the signatureAlgorithm.
+     * 
+     * @return the signatureAlgorithm
+     */
+    public AlgorithmIdentifier getSignatureAlgorithm()
+    {
+        return signatureAlgorithm;
+    }
+    
+    /**
+     * Get the signature.
+     * 
+     * @return the signature
+     */
+    public byte[] getSignature()
+    {
+        return signature;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return ToStringBuilder.reflectionToString( this, ToStringStyle.SHORT_PREFIX_STYLE );
     }
 }
--- src/main/java/de/tudarmstadt/ukp/shibhttpclient/authentication/sv/TokenData.java
+++ src/main/java/de/tudarmstadt/ukp/shibhttpclient/authentication/sv/TokenData.java
@@ -1,8 +1,12 @@
 package de.tudarmstadt.ukp.shibhttpclient.authentication.sv;
 
+import java.text.ParseException;
 import java.util.Date;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.DEREncodable;
 import org.bouncycastle.asn1.DERGeneralizedTime;
 import org.bouncycastle.asn1.DERObject;
@@ -41,4 +45,57 @@ public class TokenData implements DEREncodable
         return new DERSequence( vector );
     }
     
+    public static TokenData parse( ASN1Sequence sequence ) throws ParseException
+    {
+        if ( sequence.size() != 3 )
+        {
+            throw new IllegalArgumentException( "expected sequence of size 3" );
+        }
+        
+        DEREncodable encodedId = sequence.getObjectAt( 0 );
+        DEREncodable encodedValidFrom = sequence.getObjectAt( 1 );
+        DEREncodable encodedValidUntil = sequence.getObjectAt( 2 );
+        
+        String id = DirectoryString.getInstance( encodedId ).getString();
+        Date validFrom = DERGeneralizedTime.getInstance( encodedValidFrom ).getDate();
+        Date validUntil = DERGeneralizedTime.getInstance( encodedValidUntil ).getDate();
+        
+        return new TokenData( id, validFrom, validUntil );
+    }
+    
+    /**
+     * Get the id.
+     * 
+     * @return the id
+     */
+    public String getId()
+    {
+        return id;
+    }
+    
+    /**
+     * Get the validFrom.
+     * 
+     * @return the validFrom
+     */
+    public Date getValidFrom()
+    {
+        return validFrom;
+    }
+    
+    /**
+     * Get the validUntil.
+     * 
+     * @return the validUntil
+     */
+    public Date getValidUntil()
+    {
+        return validUntil;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return ToStringBuilder.reflectionToString( this, ToStringStyle.SHORT_PREFIX_STYLE );
+    }
 }
